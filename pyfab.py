5b6e1bc1bfe74e72d5be2a979d2d6901622c4399
Michael O'Brien <michaelobrien@nyu.edu>
2017-06-14 16:55:35 -0400

    run QGraphicsView and slm through pyfabController

--- pyfab.py
+++ pyfab.py
@@ -5,6 +5,7 @@
 from pyqtgraph.Qt import QtGui
 from traps import QTrappingPattern
 from QFabGraphicsView import QFabGraphicsView
+from pyfabController import pyfabController
 from QSLM import QSLM
 from CGH import CGH
 import sys
@@ -13,20 +14,9 @@ class pyfab(QtGui.QApplication):
 
     def __init__(self):
         super(pyfab, self).__init__(sys.argv)
-        self.fabscreen = QFabGraphicsView(size=(640,480), gray=True, mirrored=False)
-        self.fabscreen.show()
-        self.pattern = QTrappingPattern(self.fabscreen)
-        self.slm = QSLM()
-        self.slm.show()
-        self.pattern.pipeline = CGH(self.slm)
-        self.fabscreen.sigFSClosed.connect(self.cleanup)
-        #self.exec_()
-
-    def cleanup(self):
-        self.slm.close()
+        self.control = pyfabController()
+        self.exec_() 
         
-
 if __name__ == '__main__':
     import sys
-
     app = pyfab()
