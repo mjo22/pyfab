cfddc9fb1f539f82b9aed0a611f37d155abed446
= <michaelobrien@nyu.edu>
2017-11-21 13:53:31 -0500

    QFabdvr properly implements fabdvr

--- fabdvr.py
+++ fabdvr.py
@@ -18,21 +18,17 @@ class fabdvr(object):
     def record(self, nframes=None):
         if (nframes > 0):
             self._nframes = nframes
-            self.start()
+        self.start()
 
     def start(self):
         if not self.hascamera():
             return
         self.framenumber = 0
-        try:
-            self._writer = cv2.VideoWriter(self.filename,
+        self._writer = cv2.VideoWriter(self.filename,
                                            self._fourcc,
                                            self.camera.device.fps,
                                            self.size(),
                                            not self.camera.device.gray)
-        except:
-            print('did not get video writer!')
-            raise
         self.camera.sigNewFrame.connect(self.write)
 
     def stop(self):
@@ -46,10 +42,10 @@ class fabdvr(object):
         img = cv2.transpose(frame)
         img = cv2.flip(img, 0)
         self._writer.write(img)
-	print 'o'
-        #self.framenumber += 1
-        #if (self.framenumber == self._nframes):
-            #self.stop()
+        if (self._nframes > 0):
+            self.framenumber += 1
+            if (self.framenumber == self._nframes):
+                self.stop()
 
     @property
     def camera(self):
@@ -92,45 +88,45 @@ class fabdvr(object):
 
 
 class QFabdvr(QtGui.QWidget):
-    '''Widget to play and record video
+    '''Widget to implement fabdvr and record video
     '''
     def __init__(self, camera, parent=None, **kwargs):
         super(QFabdvr, self).__init__(parent, **kwargs)
+        
+        self.setGeometry(200,100,300,200)
 
-	self.setGeometry(200,100,300,200)
+        self.dvr = fabdvr(camera=camera)    #create dvr
+        
+        self._recordB = QtGui.QPushButton("Record")
+        self._stopB =  QtGui.QPushButton("Stop")    #create fields for buttons
 
-        self.dvr = fabdvr(camera=camera)
+        self.recordB = self._recordB
+        self.stopB = self._stopB    #call button setters
 
-        self.layout = QtGui.QHBoxLayout(self)
-        record = QtGui.QPushButton("Record", self)
-	record.clicked.connect(self.dvr.start)
-        stop =  QtGui.QPushButton("Stop", self)
-	record.clicked.connect(self.dvr.stop)
-        self.layout.addWidget(record)
-        self.layout.addWidget(stop)
-
-	self.setLayout(self.layout)
+        self.layout = QtGui.QHBoxLayout()
+        self.layout.addWidget(self.recordB)
+        self.layout.addWidget(self.stopB) 
+	self.setLayout(self.layout)    #set layout of QFabdvr
         self.show()
-        
-        
-	#set geometry
 
-	#init subwidgets
-        
-	#add subwidgets
-
-    #define subwidgets
-
-
+    @property
+    def recordB(self):
+        return self._recordB
     
-    def fileInput(self):
-	pass
+    @recordB.setter
+    def recordB(self, button):
+        self._recordB = button
+        self._recordB.clicked.connect(self.dvr.start)
 
-    def recorder(self):
-	pass
+    @property
+    def stopB(self):
+        return self._stopB
+    
+    @stopB.setter
+    def stopB(self, button):
+        self._stopB = button
+        self._stopB.clicked.connect(self.dvr.stop)
 
-    def replay(self):
-	pass
 
 if __name__ == '__main__':
     from PyQt4 import QtGui
