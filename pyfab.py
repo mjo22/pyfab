89ea69a5adedca11a5bcc80e00e3a5dcea8f255c
mjo22 <michaelobrien@nyu.edu>
2017-12-02 21:06:49 -0500

    following davidgrier, fix record bug

--- pyfab.py
+++ pyfab.py
@@ -101,7 +101,7 @@ class pyfab(QtGui.QMainWindow):
         return wtraps
 
     def handleRecording(self, recording):
-        self.wvideo.enabled = not recording
+        self.video.enabled = not recording
 
     def init_configuration(self):
         sz = self.fabscreen.video.device.size
